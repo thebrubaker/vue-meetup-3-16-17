3c20f4699a3150aeb9979d138e3b36e2634a14a8
Joel Brubaker <joel@tipsyelves.com>
2017-03-16 20:06:20 -0700

    Fixed bugs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import store from 'src/store'
-
-Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
 import mutations from 'src/store/mutations'
 import actions from 'src/store/actions'
 import getters from 'src/store/getters'
 import auth from 'src/store/modules/auth'
 import example from 'src/store/modules/layout'
 
-export default new Vuex({
+Vue.use(Vuex)
+
+export default new Vuex.Store({
   mutations: mutations,
   actions: actions,
   getters: getters,
